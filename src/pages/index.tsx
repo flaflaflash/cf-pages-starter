a9df7777e8697e223d4da29a4f8722df8f886fdd
flash <flash.chen2023@gmail.com>
2024-06-27 16:55:44 +0800

    feat: 🎸 use edge

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
-
+export const runtime = 'edge'
 export default function Home() {
   return (
     <main
