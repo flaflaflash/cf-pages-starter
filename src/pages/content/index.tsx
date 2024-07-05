3d8cc3bb13e0412d5a5192f68128c225c3ee1761
flash <flash.chen2023@gmail.com>
2024-07-05 15:00:15 +0800

    feat: content add getServerSideProps

--- src/pages/content/index.tsx
+++ src/pages/content/index.tsx
@@ -1,6 +1,24 @@
+import { GetServerSideProps } from "next";
+import { getCookie } from "cookies-next";
+
 export const config = {
 	runtime: 'experimental-edge',
 };
 export default function Content() {
   return <div>this is content page</div>;
 }
+
+export const getServerSideProps: GetServerSideProps = async ({
+  locale,
+  req,
+  res,
+  query,
+}) => {
+  const isDark = getCookie("is_night", { req, res });
+
+  return {
+    props: {
+      isDark,
+    },
+  };
+};
