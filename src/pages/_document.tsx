7f854cfe3f98ea2815bf6e3f5eec654ea8c2e1a3
flash <flash.chen2023@gmail.com>
2024-07-05 14:00:30 +0800

    feat: 🎸 fix build err

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,4 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import "@/public/style.css"
 
 export default function Document(props: {
   __NEXT_DATA__: { props: { pageProps: any } };
